675d2540b0191250123822fbe55785240e04e72c
ebarriov <estelabarriovillegas@gmail.com>
2022-02-28 18:17:39 +0100

    deleted comments on pingpong game

--- src/ros_basics/scripts/pong_pubsub.py
+++ src/ros_basics/scripts/pong_pubsub.py
@@ -1,24 +1,6 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import String
-from std_msgs.msg import Int64
-
-#def callback_pong(msg):
-#	if msg.data == 'ping':
-#		output = String
-#		output.data = 'pong'
-#		pub.publish(output)
-#		rospy.loginfo("data: %s", output)
-#	else:
-#		output.data = 'Fail'
-#		pub.publish(output)
-#		rospy.loginfo("data: %s", output)
-
-#rospy.init_node('pong_node')
-#pub = rospy.Publisher("/pong", String, queue_size=10)
-#sub = rospy.Subscriber("/ping", String, callback_pong)
-#rospy.spin()
-
 
 output = 0
 
